4271e93ad66cd427ab2e191a6e8638068d3f22bf
marcturu <marc.turu@estudiantat.upc.edu>
2025-08-06 14:01:58 +0200

    Telegram message update

--- main.py
+++ main.py
@@ -219,7 +219,7 @@ async def send_alert(trades):
             message += (
                 f"\n✅ {ticker} ({type_upper}) by {company}"
                 f"\n👤 Politician: {politician}"
-                f"\n📅 Date: {row['Date'].date()}"
+                f"\n📅 Trade Date: {row['Date'].date()}"
                 f"\n🗓️ Publication Date: {row['PublicationDate'].strftime('%d/%m/%Y %H:%M') if pd.notnull(row['PublicationDate']) else 'N/A'}"
                 f"\n💰 Amount: ${row['Amount']:,.0f}"
             )
